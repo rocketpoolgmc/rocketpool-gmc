feff5a21239e30410f3a182869c25a7b4c45a4d8
ShfRyn <shfryn@gmail.com>
2023-09-26 10:29:30 -0400

    creating folder structure

---
+++ bounties.js
@@ -0,0 +1,32 @@
+const bounties = [
+  {
+    title: 'FAQ Document',
+    code: 'BA012301',
+    date: 'Feb 1st 2023',
+    proposer: 'Valdorff',
+    amount: '53 RPL',
+    value: '$38 USD',
+    details:
+      'This is where the description of the bounty would go. Ideally it would be a short paragraph that explains the bounty & gets the user interested in applying for it.',
+    fineprint:
+      'Answers must be provided in both article / doc page PR form; pot can be refreshed by GMC if it runs low - idea was $100/answer',
+    status: 'Open',
+    url: 'https://dao.rocketpool.net/t/january-2023-gmc-call-for-bounty-applications-deadline-is-january-15th/1336/12',
+    linkTitle: 'Apply',
+  },
+  {
+    title: 'FAQ Document',
+    code: 'BA012301',
+    date: 'Feb 1st 2023',
+    proposer: 'Valdorff',
+    amount: '53 RPL',
+    value: '$38 USD',
+    details:
+      'This is where the description of the bounty would go. Ideally it would be a short paragraph that explains the bounty & gets the user interested in applying for it.',
+    fineprint:
+      'Answers must be provided in both article / doc page PR form; pot can be refreshed by GMC if it runs low - idea was $100/answer',
+    status: 'Open',
+    url: 'https://dao.rocketpool.net/t/january-2023-gmc-call-for-bounty-applications-deadline-is-january-15th/1336/12',
+    linkTitle: 'Apply',
+  },
+];
